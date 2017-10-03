251eb24d598b3bcd4b16ae77b803ca1e37e8819d
Sterling Parker <caligari.87@gmail.com>
2017-10-03 17:53:55 -0600

    Map light functions + wrapper for tile index math

--- map.cpp
+++ map.cpp
@@ -11,56 +11,67 @@ class TileData {
 
 		bool Seen=false;
 		bool Visible=false;
+		int LightLevel=1;
 };
 
 class MapData {
 	public:
 		TileData Tiles[MaxRows*MaxCols];
 
+		int XYToFlat(int row, int col) {
+			return (XYToFlat(row,col));
+		}
+
+		//Information functions
 		bool IsWall(int row, int col) {
-			return(!Tiles[row+col*MaxRows].CanWalk && Tiles[row+col*MaxRows].BlocksVision && Tiles[row+col*MaxRows].Symbol=='#');
+			return(!Tiles[XYToFlat(row,col)].CanWalk && Tiles[XYToFlat(row,col)].BlocksVision && Tiles[XYToFlat(row,col)].Symbol=='#');
 		}
 
 		bool IsDoor(int row, int col) {
-			return(Tiles[row+col*MaxRows].CanWalk && Tiles[row+col*MaxRows].BlocksVision);
+			return(Tiles[XYToFlat(row,col)].CanWalk && Tiles[XYToFlat(row,col)].BlocksVision);
 		}
 
 		bool CanWalk(int row, int col) {
-			return(Tiles[row+col*MaxRows].CanWalk);
+			return(Tiles[XYToFlat(row,col)].CanWalk);
 		}
 
 		bool BlocksVision(int row, int col) {
-			return(Tiles[row+col*MaxRows].BlocksVision);
+			return(Tiles[XYToFlat(row,col)].BlocksVision);
 		}
 
 		bool Seen(int row, int col) {
-			return(Tiles[row+col*MaxRows].Seen);
+			return(Tiles[XYToFlat(row,col)].Seen);
 		}
 
 		bool Visible(int row, int col) {
-			return(Tiles[row+col*MaxRows].Visible);
+			return(Tiles[XYToFlat(row,col)].Visible);
 		}
 
 		char Symbol(int row, int col) {
-			return(Tiles[row+col*MaxRows].Symbol);
+			return(Tiles[XYToFlat(row,col)].Symbol);
 		}
 
+		int LightLevel(int row, int col) {
+			return(Tiles[XYToFlat(row,col)].LightLevel);
+		}
+
+		//Setting functions
 		void SetWall(int row, int col) {
-			Tiles[row+col*MaxRows].CanWalk = false;
-			Tiles[row+col*MaxRows].BlocksVision = true;
-			Tiles[row+col*MaxRows].Symbol = '#';
+			Tiles[XYToFlat(row,col)].CanWalk = false;
+			Tiles[XYToFlat(row,col)].BlocksVision = true;
+			Tiles[XYToFlat(row,col)].Symbol = '#';
 		}
 
 		void SetFloor(int row, int col) {
-			Tiles[row+col*MaxRows].CanWalk = true;
-			Tiles[row+col*MaxRows].BlocksVision = false;
-			Tiles[row+col*MaxRows].Symbol = '.';
+			Tiles[XYToFlat(row,col)].CanWalk = true;
+			Tiles[XYToFlat(row,col)].BlocksVision = false;
+			Tiles[XYToFlat(row,col)].Symbol = '.';
 		}
 
 		void SetDoor(int row, int col) {
-			Tiles[row+col*MaxRows].CanWalk = true;
-			Tiles[row+col*MaxRows].BlocksVision = true;
-			Tiles[row+col*MaxRows].Symbol = '+';
+			Tiles[XYToFlat(row,col)].CanWalk = true;
+			Tiles[XYToFlat(row,col)].BlocksVision = true;
+			Tiles[XYToFlat(row,col)].Symbol = '+';
 		}
 
 		void SetAllNotVisible() {
@@ -70,10 +81,14 @@ class MapData {
 		}
 
 		void SetVisibleState(int row, int col, bool value) {
-			Tiles[row+col*MaxRows].Visible = value;
+			Tiles[XYToFlat(row,col)].Visible = value;
 		}
 
 		void SetSeenState(int row, int col, bool value) {
-			Tiles[row+col*MaxRows].Seen = value;
+			Tiles[XYToFlat(row,col)].Seen = value;
+		}
+
+		void SetLightLevel(int row, int col, int value) {
+			Tiles[XYToFlat(row,col)].LightLevel = value;
 		}
 };
